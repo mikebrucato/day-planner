af75859c1038bbec3834da66b63c3d30105734b2
mikebrucato <brucatomike1@gmail.com>
2020-10-30 18:56:01 -0500

    added variables and array for time

---
+++ script.js
@@ -0,0 +1,7 @@
+// date variables and time of day array
+
+var date = moment().format("MMM do YY")
+
+$("#currentDay").append(date)
+
+var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]

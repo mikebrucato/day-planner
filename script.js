e1e6507644c98d57771576cc19598b84d04a0488
mikebrucato <brucatomike1@gmail.com>
2020-10-30 20:48:06 -0500

    added for loop, functions, and if else statements toi change the color of the background

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // date variables and time of day array
 
-var date = moment().format("MMM do YY")
+var date = moment().format("MMMM do YYYY")
 
 $("#currentDay").append(date)
 
+
+// function that updates the 
 var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
+
+updateTime();
+
+function updateTime() {
+    var currentTime = moment().format('H')
+        for (var i = 0; i < timeOfDay.length; i++)
+
+            if (parseInt(timeOfDay[i]) > currentTime) {
+                $("#" + timeOfDay[i]).attr("style", "background-color: #58ce7b") 
+
+            }
+
+            else if (parseInt(timeOfDay[i]) < currentTime) {
+                $("#" + timeOfDay[i]).attr("style", "background-color: lightgray")
+
+            }
+
+            else if (parseInt(timeOfDay[i]) == currentTime) {
+                $("#" + timeOfDay[i]).attr("style", "background-color: #fc665e")
+            }
+}
